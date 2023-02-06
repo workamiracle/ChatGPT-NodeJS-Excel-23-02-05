150e520a8a869f31c157d010fc7881ed2ddbf1f7
LonelyGuyPiny <devops7816@protonmail.com>
2023-02-06 15:57:58 -0500

    Project Panel, Task Panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="Project-panel">
+          <button className="btn btn-primary btn-lg">+ Create Project</button>
+
+          <table className="table table-info table-striped mt-4">
+            <thead>
+              <tr className="table-primary">
+                <th>Porject Name</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Porject1(Google Doc)</td>
+                <td>Rename</td>
+                <td>Delete</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="Task-panel mt-5">
+          <h2>Project 1</h2>
+
+          <button className="btn btn-primary btn-lg mt-3">+ Create Task</button>
+
+          <table className="table table-info table-striped mt-4">
+            <thead>
+              <tr className="table-primary">
+                <th>Task Name</th>
+                <th>Type</th>
+                <th>Action</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Task 1 (Worksheet)</td>
+                <td>Fixed</td>
+                <td>Run</td>
+                <td>Rename</td>
+                <td>Delete</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
